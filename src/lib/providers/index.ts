d751ba9a1dd21b2d385079eb04be4bdee063c246
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-04 12:58:41 +0700

    Delete src directory

--- src/lib/providers/index.ts
+++
@@ -1,7 +0,0 @@
-
-export interface GenerateParams { prompt: string; imageUrl?: string; }
-export interface Provider {
-  name: string;
-  generate(params: GenerateParams): Promise<string>;
-}
-export { openaiProvider } from "./openai";
