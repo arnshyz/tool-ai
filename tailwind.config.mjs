122d09db698e6b0fecf8cc9ed8a8c5b5705ec611
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-04 13:23:31 +0700

    Delete tailwind.config.mjs

--- tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/app/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
-  theme: { extend: {} },
-  plugins: []
-};
